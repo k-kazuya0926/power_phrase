584b15b846e0bd92a5b1b819c752e88e6bcba77f
k-kazuya0926 <61341861+k-kazuya0926@users.noreply.github.com>
2020-05-04 13:16:25 +0900

    SSL対応

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,24 +12,12 @@ class TrustProxies extends Middleware
      *
      * @var array|string
      */
-    protected $proxies = '**';
+    protected $proxies = '54.178.0.0/16';
 
     /**
      * The headers that should be used to detect proxies.
      *
      * @var int
      */
-    // protected $headers = Request::HEADER_X_FORWARDED_ALL;
-    /**
-     * The current proxy header mappings.
-     *
-     * @var array
-     */
-    protected $headers = [
-        Request::HEADER_FORWARDED => 'FORWARDED',
-        Request::HEADER_X_FORWARDED_FOR => 'X_FORWARDED_FOR',
-        Request::HEADER_X_FORWARDED_HOST => 'X_FORWARDED_HOST',
-        Request::HEADER_X_FORWARDED_PORT => 'X_FORWARDED_PORT',
-        Request::HEADER_X_FORWARDED_PROTO => 'X_FORWARDED_PROTO',
-    ];
+    protected $headers = Request::HEADER_X_FORWARDED_ALL;
 }
