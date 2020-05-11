f7b7f792d7b8d5d791b996b4c96a791512779923
k-kazuya0926 <61341861+k-kazuya0926@users.noreply.github.com>
2020-05-11 22:56:26 +0900

    Bootstrap導入

--- resources/views/entries/index.blade.php
+++ resources/views/entries/index.blade.php
@@ -3,47 +3,43 @@
 @section('title', 'Entries')
 
 @section('content')
-<div class="uk-container">
-    <h1 class="uk-text-center">Power Phrase</h1>
-    <p class="uk-text-center">あなたを支える言葉を共有しよう！</p>
-    <img class="uk-align-center" data-src="images/3192174_s.jpg" alt="" uk-img>
-    <h3 class="uk-text-center">投稿一覧</h3>
-    <div class="uk-child-width-1-3@m uk-child-width-1-1@s uk-grid-match" uk-grid>
+<div class="container">
+    <h1 class="text-center">Power Phrase</h1>
+    <p class="text-center">あなたを支える言葉を共有しよう！</p>
+    <img class="mx-auto d-block mb-5" src="images/3192174_s.jpg" alt="">
+    <h3 class="text-center mb-3">投稿一覧</h3>
+    <div class="card-deck">
         @forelse ($entries as $entry)
-            <div>
-                <div class="uk-card uk-card-default">
-                    <div class="uk-card-header">
-                        <div class="uk-grid-small uk-flex-middle" uk-grid>
-                            <div class="uk-width-expand">
-                                <h3 class="uk-card-title uk-margin-remove-bottom">
-                                    <a href="{{ action('EntriesController@show', $entry) }}">
-                                        {{ $entry->power_phrase }}
-                                        @if (!empty($entry->source))({{ $entry->source }})@endif
-                                    </a>
-                                </h3>
-                                <span class="uk-text-meta uk-margin-remove-top">{{ $entry->user->name }} <time datetime="2016-04-01T19:00">2020/05/03</time></span>
-                                @if ($entry->user_id == Auth::id())
-                                <a href="{{ action('EntriesController@edit', $entry) }}" class="edit">[{{ __('Edit') }}]</a>
-                                <a href="#" class="del" data-id="{{ $entry->id }}" onclick="
-                                    event.preventDefault();
-                                    if (confirm('削除しますか？')) {
-                                        document.getElementById('form_{{ $entry->id }}').submit();
-                                    }    
-                                ">
-                                    [{{ __('Delete') }}]
-                                </a>
-                                <form method="post" action="{{ url('/entries', $entry->id) }}" id="form_{{ $entry->id }}">
-                                    @csrf
-                                    {{ method_field('delete') }}
-                                </form>
-                                @endif
-                            </div>
-                        </div>
+            <div class="col-12 col-sm-4 mb-5">
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title">
+                            <a href="{{ action('EntriesController@show', $entry) }}">
+                                {{ $entry->power_phrase }}
+                                @if (!empty($entry->source))({{ $entry->source }})@endif
+                            </a>
+                        </h3>
+                        <span class="text-muted">{{ $entry->user->name }} {{ $entry->created_at }}</span>
+                        @if ($entry->user_id == Auth::id())
+                        <a href="{{ action('EntriesController@edit', $entry) }}" class="edit">[{{ __('Edit') }}]</a>
+                        <a href="#" class="del" data-id="{{ $entry->id }}" onclick="
+                            event.preventDefault();
+                            if (confirm('削除しますか？')) {
+                                document.getElementById('form_{{ $entry->id }}').submit();
+                            }    
+                        ">
+                            [{{ __('Delete') }}]
+                        </a>
+                        <form method="post" action="{{ url('/entries', $entry->id) }}" id="form_{{ $entry->id }}">
+                            @csrf
+                            {{ method_field('delete') }}
+                        </form>
+                        @endif
                     </div>
-                    <div class="uk-card-body">
+                    <div class="card-body">
                         <p>{{ $entry->episode }}</p>
                     </div>
-                    <div class="uk-card-footer">
+                    <div class="card-footer">
                         {{ __('Comment') }}{{ $entry->comments->count() }}件
                     </div>
                 </div>
@@ -52,9 +48,8 @@
         <li>No entries yet</li>
         @endforelse
     </div>
-    <div class="d-flex justify-content-center uk-margin-top">
+    <div class="d-flex justify-content-center">
         {{ $entries->links() }}
     </div>
 </div>
-{{-- <script src="/js/main.js"></script> --}}
 @endsection
